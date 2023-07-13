2dbd8dc561fd692cd5cf586433c2a3979c417ea3
Ashraf Mohammed <21951A6612@iare.ac.in>
2023-07-13 21:17:54 +0530

    add todo

--- javascript/task.js
+++ javascript/task.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+//TODO create help and log it when /help or ./task is called
+//TODO create /ls command to display all tasks
+//TODO create /add [task][priority] command to add a task
+//TODO create /done [number] command to mark a task as done
+//TODO create /del [number] command to remove a task
+//TODO create /report command to display pending and completed tasks
